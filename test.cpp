5398c76c10c4772a1ad2fd7154ed0f1abce939db
yelin0323 <48678537+yelin0323@users.noreply.github.com>
2020-11-28 02:02:50 +0900

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Main.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	if (window.isOpen())
+	{
+		window.close();
+		int test = 0;
+	}
+
+	return 0;
+}
